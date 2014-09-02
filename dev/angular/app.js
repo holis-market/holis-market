2aea75142b959342b97af9da67f2dd0f52954786
BelindaSwbd <Belinda@Belinda-Swobo.local>
2014-09-02 21:25:46 +0200

    Testimonial-Seiten angelegt

--- dev/angular/app.js
+++ dev/angular/app.js
@@ -27,8 +27,33 @@
 		.when('/testimonial-eipeltauer', { templateUrl : 'pages/testimonials/testimonial-eipeltauer.html' })
 		.when('/testimonial-seyr', { templateUrl : 'pages/testimonials/testimonial-seyr.html' })
 		.when('/testimonial-reisinger', { templateUrl : 'pages/testimonials/testimonial-reisinger.html' })
-		
-		;
+		.when('/testimonial-leplay', { templateUrl : 'pages/testimonials/testimonial-leplay.html' })
+		.when('/testimonial-rosemanna', { templateUrl : 'pages/testimonials/testimonial-rosemanna.html' })
+		.when('/testimonial-kivrak', { templateUrl : 'pages/testimonials/testimonial-kivrak.html' })
+		.when('/testimonial-ortner', { templateUrl : 'pages/testimonials/testimonial-ortner.html' })
+		.when('/testimonial-furdea', { templateUrl : 'pages/testimonials/testimonial-furdea.html' })
+		.when('/testimonial-gruenzweil', { templateUrl : 'pages/testimonials/testimonial-gruenzweil.html' })
+		.when('/testimonial-reitmayr', { templateUrl : 'pages/testimonials/testimonial-reitmayr.html' })
+		.when('/testimonial-brandstaetter', { templateUrl : 'pages/testimonials/testimonial-brandstaetter.html' })
+		.when('/testimonial-frischefritzen', { templateUrl : 'pages/testimonials/testimonial-frischefritzen.html' })
+		.when('/testimonial-gin', { templateUrl : 'pages/testimonials/testimonial-gin.html' })
+		.when('/testimonial-mayr', { templateUrl : 'pages/testimonials/testimonial-mayr.html' })
+		.when('/testimonial-kollmann', { templateUrl : 'pages/testimonials/testimonial-kollmann.html' })
+		.when('/testimonial-roth', { templateUrl : 'pages/testimonials/testimonial-roth.html' })
+		.when('/testimonial-moser', { templateUrl : 'pages/testimonials/testimonial-moser.html' })
+		.when('/testimonial-schuetzenhofer', { templateUrl : 'pages/testimonials/testimonial-schuetzenhofer.html' })
+		.when('/testimonial-stitz', { templateUrl : 'pages/testimonials/testimonial-stitz.html' })
+		.when('/testimonial-lehner', { templateUrl : 'pages/testimonials/testimonial-lehner.html' })
+		.when('/testimonial-rabitsch', { templateUrl : 'pages/testimonials/testimonial-rabitsch.html' })
+		.when('/testimonial-sternad', { templateUrl : 'pages/testimonials/testimonial-sternad.html' })
+		.when('/testimonial-rosemanne', { templateUrl : 'pages/testimonials/testimonial-rosemanne.html' })
+		.when('/testimonial-ly', { templateUrl : 'pages/testimonials/testimonial-ly.html' })
+		.when('/testimonial-reinberger', { templateUrl : 'pages/testimonials/testimonial-reinberger.html' })
+		.when('/testimonial-weichert', { templateUrl : 'pages/testimonials/testimonial-weichert.html' })
+		.when('/testimonial-greil', { templateUrl : 'pages/testimonials/testimonial-greil.html' })
+
+
+;
 		// use the HTML5 History API
 		//$locationProvider.html5Mode(true);
 	});
